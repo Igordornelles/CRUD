981d34ea0ab7821dc00a7b94348d2e956fccec5b
Igor Dornelles <igordornelles94@gmail.com>
2025-06-09 12:14:31 -0300

    Atualização do crud

--- Asp.net/M_V_C/ControleContatos/ControleContatos/obj/Debug/net9.0/ControleContatos.AssemblyInfo.cs
+++ Asp.net/M_V_C/ControleContatos/ControleContatos/obj/Debug/net9.0/ControleContatos.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ControleContatos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2bd804cf60917b80531e426e63359e29b1c1c924")]
 [assembly: System.Reflection.AssemblyProductAttribute("ControleContatos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ControleContatos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
